e4271769bc051eb26ccdc228edd049be36080100
Macheya Steele <mcheysteele@gmail.com>
2021-01-05 18:52:19 -0700

    added time var

--- Develop/script.js
+++ Develop/script.js
@@ -2,4 +2,15 @@
 var currentDate = moment();
 currentDay.textContent = currentDate.format("dddd, MMMM Do YYYY");
 
-
+var workday = {
+    "8 AM": "",
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+}
