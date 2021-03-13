efabbb9879ac9675e2b76e9ec2621d432b664ce2
Macheya Steele <mcheysteele@gmail.com>
2021-03-13 10:52:05 -0700

    redid event listener, retrieved time blocks, added some logic for time tracker

--- Develop/script.js
+++ Develop/script.js
@@ -2,28 +2,48 @@
 var currentDate = moment();
 currentDay.textContent = currentDate.format("dddd, MMMM Do YYYY");
 
-var workday = {
-    "8 AM": "",
-    "9 AM": "",
-    "10 AM": "",
-    "11 AM": "",
-    "12 PM": "",
-    "1 PM": "",
-    "2 PM": "",
-    "3 PM": "",
-    "4 PM": "",
-    "5 PM": "",
+//var workday = {
+ //   "8 AM": "",
+   // "9 AM": "",
+   // "10 AM": "",
+   // "11 AM": "",
+   // "12 PM": "",
+   // "1 PM": "",
+ //   "2 PM": "",
+ //   "3 PM": "",
+ //   "4 PM": "",
+  //  "5 PM": "",
 }
 
 //save function
-var buttonClickHandler = function (event) {
-console.log("It worked");
+
+
+//Save button event listener()
+$(document).ready (function () {
+    $("#save-btn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //saving in local storage
+        localStorage.setItem(time, text);
+    
+    });
 }
 
-//Save button event listener
-document.getElementById("save-btn").addEventListener("click", buttonClickHandler);
+
 
 
 
 //Time Block Functions
 
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    $("#hour6 .description").val(localStorage.getItem("hour6"));
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
