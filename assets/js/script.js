7116d33fe67c956b7cdfe9519686d9ea7c4a6b10
Macheya Steele <mcheysteele@gmail.com>
2021-03-15 14:02:39 -0600

    fixed storage functionality and color code

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,42 +19,47 @@ currentDay.textContent = currentDate.format("dddd, MMMM Do YYYY");
 
 
 //Save button event listener()
-$(document).ready (function () {
-    $("saveBtn").on("click", function () {
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         //saving in local storage
         localStorage.setItem(time, text);
-    
+        console.log(time, text);
 })
 
 
 
 
-function tracker () {
+function timeTracker () {
     
     var timeNow = moment().hour();
-
+    console.log(timeNow);
             
             $(".time-block").each(function () {
+                console.log($(this));
+
                 var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-    
+                console.log(blockTime);
                 
                 if (blockTime < timeNow) {
-                    $(this).removeClass("future");
-                    $(this).removeClass("present");
-                    $(this).addClass("past");
+                    $(this).children(".description").addClass("past");
+                    $(this).children(".description").removeClass("future");
+                    $(this).children(".description").removeClass("present");
+                    
                 }
                 else if (blockTime === timeNow) {
-                    $(this).removeClass("past");
-                    $(this).removeClass("future");
-                    $(this).addClass("present");
+                    $(this).children(".description").addClass("present");
+                    $(this).children(".description").removeClass("past");
+                    $(this).children(".description").removeClass("future");
+                    
                 }
                 else {
-                    $(this).removeClass("present");
-                    $(this).removeClass("past");
-                    $(this).addClass("future");
+                    $(this).children(".description").addClass("future");
+                    $(this).children(".description").removeClass("present");
+                    $(this).children(".description").removeClass("past");
+                    
     
                 }
             })
@@ -64,15 +69,15 @@ function tracker () {
 
 
 
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    $("#hour6 .description").val(localStorage.getItem("hour6"));
-    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
-tracker();
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+timeTracker();
 })
